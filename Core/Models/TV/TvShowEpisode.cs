4e4038b0ebd69bf1646d0c3e9a382caedbd30d3a
midnightgizmo <scottamillard@hotmail.com>
2022-01-14 11:46:06 +0000

    Add to Core Project: Models to hold tv data from database

---
+++ Core/Models/TV/TvShowEpisode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models.TV
+{
+    public class TvShowEpisode
+    {
+
+        /// <summary>
+        /// Called "strFileName" in database. e.g. "To the end s01e04.mp4"
+        /// </summary>
+        public string FileName { get; set; }
+
+
+        /// <summary>
+        /// Called "playCount" in the database.
+        /// Number of times the episode has been watched. If zero, the episode has not been watched
+        /// </summary>
+        public int WatchedCount { get; set; }
+    }
+}
