93db0588d3878d59b6e98896b6c1a7b37bcc5dc3
midnightgizmo <scottamillard@hotmail.com>
2022-01-14 12:32:58 +0000

    Core project: add season num & Epp num to TvShowEpisode model class

--- Core/Models/TV/TvShowEpisode.cs
+++ Core/Models/TV/TvShowEpisode.cs
@@ -12,13 +12,23 @@ namespace Core.Models.TV
         /// <summary>
         /// Called "strFileName" in database. e.g. "To the end s01e04.mp4"
         /// </summary>
-        public string FileName { get; set; }
+        public string FileName { get; set; } = string.Empty;
 
 
         /// <summary>
         /// Called "playCount" in the database.
         /// Number of times the episode has been watched. If zero, the episode has not been watched
         /// </summary>
-        public int WatchedCount { get; set; }
+        public int WatchedCount { get; set; } = 0;
+
+
+        /// <summary>
+        /// The season number this eppisode is from (c12 in database)
+        /// </summary>
+        public int SeasonNumber { get; set; } = -1;
+        /// <summary>
+        /// The eppisode number within the season (c13 in database)
+        /// </summary>
+        public int SeasonEppNumber { get; set; } = -1;
     }
 }
