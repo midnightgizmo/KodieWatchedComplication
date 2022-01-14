edda708d26ce953c713f85ce0b83983f0a638e56
midnightgizmo <scottamillard@hotmail.com>
2022-01-14 11:30:40 +0000

    Add Global Settings class to Core project

---
+++ Core/Data/Settings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data
+{
+    public class Settings
+    {
+        public static dbSettings DatabaseLogin { get; set; }
+
+        static Settings()
+        {
+            DatabaseLogin = new dbSettings();
+        }
+    }
+
+    public class dbSettings
+    {
+        public string ServerLocation { get; set; }
+        public string dbUser { get; set; }
+        public string dbPassword { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
