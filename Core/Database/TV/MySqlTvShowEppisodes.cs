845ac54e48ad201e74cf8c30f68a55441051473b
midnightgizmo <scottamillard@hotmail.com>
2022-01-14 13:09:23 +0000

    Add Core project: Ability to get all shows for a tv show from database

---
+++ Core/Database/TV/MySqlTvShowEppisodes.cs
@@ -0,0 +1,53 @@
+﻿using Core.Models.TV;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Database.TV
+{
+    public class MySqlTvShowEppisodes
+    {
+        public readonly string dbTableName = "episode_view";
+        public List<TvShowEpisode> SelectAllTvEpisodes(int TvShowID)
+        {
+            List<TvShowEpisode> tvShowEppisodes = new List<TvShowEpisode>();
+            dbMySqlConnection con = new dbMySqlConnection();
+            bool wasLoginSucsefull = false;
+            StringBuilder sb = new StringBuilder();
+            MySqlDataReader rdr;
+
+            wasLoginSucsefull = con.OpenConnection();
+
+            // if we can't login to the database, return an empty list
+            if (wasLoginSucsefull == false)
+                return tvShowEppisodes;
+
+            sb.Append("SELECT c12 as SeasonNumber, c13 as EpisodeNumber, strFileName as FileName FROM ");
+            sb.Append(this.dbTableName + " ");
+            sb.Append("WHERE idShow=:TvShowID;");
+
+            MySqlParameter param = new MySqlParameter(":TvShowID",MySqlDbType.Int32);
+            param.Value = TvShowID;
+
+            rdr = con.ExecuteParameterizedSelectCommand(sb.ToString(), new MySqlParameter[] { param });
+
+            while (rdr.Read())
+            {
+                TvShowEpisode tvEpp = new TvShowEpisode();
+                tvEpp.SeasonNumber = rdr.GetInt32("SeasonNumber");
+                tvEpp.SeasonEppNumber = rdr.GetInt32("EpisodeNumber");
+                tvEpp.FileName = rdr.GetString("FileName");
+
+                tvShowEppisodes.Add(tvEpp);
+            }
+
+            rdr.Close();
+            con.CloseConnection();
+
+            return tvShowEppisodes;
+        }
+    }
+}
